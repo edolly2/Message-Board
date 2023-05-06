e3a1ad55a5c2410e3fc96418487f6dca8e723c6e
dollyllama <edolly2@protonmail.com>
2023-05-05 20:03:22 -0500

    "Complete"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    content: 'Hi there!',
+    user: 'Brian',
+    date: new Date(),
+  },
+  {
+    content: 'Hello World!',
+    user: 'Eric',
+    date: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  const button = `<a href='/new'><button>New Message</button></a>`;
+  res.render('index', { title: 'Mini Messageboard', messages, button });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Send Message' });
+});
+
+router.post('/new', (req, res) => {
+  const messageName = req.body.name;
+  const messageText = req.body.message;
+
+  messages.push({ content: messageText, user: messageName, date: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
